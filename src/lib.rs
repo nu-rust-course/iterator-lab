8164db591b69ff4eda9a71b2ef9bc25628eb9d87
Jesse A. Tov <jesse.tov@gmail.com>
2021-04-07 17:34:29 -0500

    mehhh

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,3 @@
 mod helpers;
 
 pub mod bees;
-
-pub mod crusty_rustys;
-
-pub mod go_huskies;
-
-pub mod golden_retrievers;
-
-pub mod man_vs_code {
-    pub mod it_encoder;
-    pub mod string_encoder;
-}
-
-pub mod monkeynator;
-
-
